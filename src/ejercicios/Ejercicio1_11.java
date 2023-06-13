2efcdd49ff92b700e72fae7105c454f1ead23dab
Y <Y@DESKTOP-KJUN4CO>
2023-06-13 13:52:22 +0200

    commit inicial

--- src/ejercicios/Ejercicio1_11.java
+++ src/ejercicios/Ejercicio1_11.java
@@ -1,10 +1,29 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio1_11 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		// Programa que lee un número de 3 cifras y muestra sus cifras por separado. 
+		
+Scanner teclado = new Scanner(System.in);
+		
+		float Numero1;
+		
+		
+		System.out.println("Introduce el radio de la esfera ");
+		Numero1 = teclado.nextFloat();
+		
+		float calculo = Numero1/10000;
+		float calculo2 = calculo/1000;
+		float calculo3 = calculo2/100;
+		float calculo4 = calculo3/10;
+		float calculo5 = calculo4/1;
+		
+		System.out.println("El volumen de la esfera es: "+ volumen); 
 
+		teclado.close();
 	}
 
 }
